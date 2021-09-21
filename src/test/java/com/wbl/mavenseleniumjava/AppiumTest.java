5291b7b1c5001d1a96ed730d94195db062012175
bhavana <bhavana.rayabandi@gmail.com>
2021-09-20 20:37:50 -0600

    first commit

---
+++ src/test/java/com/wbl/mavenseleniumjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package com.wbl.mavenseleniumjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+		@Test
+		public void NativeApiAndroid() {
+			System.out.println("NativeApiAndroid() in AppiumTest");
+		}
+		
+		@Test
+		public void IosApps() {
+			System.out.println("IosApps() in AppiumTest");
+		}
+		
+}
---
+++ src/test/java/com/wbl/mavenseleniumjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package com.wbl.mavenseleniumjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+		@Test
+		public void BrowserAutomation() {
+			System.out.println(" BrowserAutomation() in SeleniumTest");
+		}
+		
+		@Test
+		public void elementsUi() {
+			System.out.println("elementsUi() in SeleniumTest" );
+		}
+}
